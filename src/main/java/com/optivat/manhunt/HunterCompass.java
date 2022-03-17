509cdfc6ed77a78fae82fceb9e3ece34e32dfc65
Optivat <67515969+Optivat@users.noreply.github.com>
2022-03-16 22:53:10 -0700

    test

--- src/main/java/com/optivat/manhunt/HunterCompass.java
+++ src/main/java/com/optivat/manhunt/HunterCompass.java
@@ -3,6 +3,7 @@ package com.optivat.manhunt;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class HunterCompass implements Listener {
@@ -18,5 +19,9 @@ public class HunterCompass implements Listener {
             main.speedrunners.replace(p, p.getLocation());
         }
     }
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
 
+    }
 }
+
