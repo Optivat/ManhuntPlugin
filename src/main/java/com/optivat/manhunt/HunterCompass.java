6d9ae607acb1e3efbca3d7ef8939229b32f6a89b
Optivat <67515969+Optivat@users.noreply.github.com>
2022-03-16 23:08:54 -0700

    Added if the player leaves and rejoins, making sure that they don't leave the assigned team.

--- src/main/java/com/optivat/manhunt/HunterCompass.java
+++ src/main/java/com/optivat/manhunt/HunterCompass.java
@@ -5,29 +5,41 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.HashMap;
 
 public class HunterCompass implements Listener {
+    HashMap<Player, Player> compassSelection = new HashMap<>();
+
+
     Manhunt main;
     public HunterCompass(Manhunt main) {
         this.main = main;
     }
 
     @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        if (!(main.speedrunners.containsKey(e.getPlayer()) && compassSelection.containsKey(e.getPlayer()))) {
+            compassSelection.put(e.getPlayer(), null);
+        }
+    }
+
+
+
+    @EventHandler
     public void onMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
+        //Gets speedrunner's location
         if(main.speedrunners.containsKey(p)) {
             main.speedrunners.replace(p, p.getLocation());
         }
     }
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
-        if(e.getAction().equals(Action.LEFT_CLICK_AIR)) {
 
-        }
-        if(e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-
-        }
     }
 }
 
