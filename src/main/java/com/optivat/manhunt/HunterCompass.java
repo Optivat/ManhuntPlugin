8e6d52d4063ba9e2d0026cabd8d4b47d61993fa0
Optivat <67515969+Optivat@users.noreply.github.com>
2022-03-16 22:56:25 -0700

    Added PlayerInteractEvents

--- src/main/java/com/optivat/manhunt/HunterCompass.java
+++ src/main/java/com/optivat/manhunt/HunterCompass.java
@@ -3,6 +3,7 @@ package com.optivat.manhunt;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
@@ -21,7 +22,12 @@ public class HunterCompass implements Listener {
     }
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
+        if(e.getAction().equals(Action.LEFT_CLICK_AIR)) {
 
+        }
+        if(e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+
+        }
     }
 }
 
